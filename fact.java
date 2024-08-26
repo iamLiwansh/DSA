664f5c736f9e4834e97b9b236348e4d02aecb669
iamLiwansh <saini.liwansh@gmail.com>
2024-08-26 13:26:19 +0530

    by recursion

--- fact.java
+++ fact.java
@@ -7,8 +7,20 @@ public class fact {
         }
         return total;
     }
+    //recursive
+    public static int recursive(int n){
+        
+        if(n==1){
+            return 1;
+        }
+        
+        
+
+        return n * recursive (n-1);
+    }
     public static void main (String[] args){
         int n = 5;
         System.out.println(factItra(n));
+        System.out.print(recursive( n));
     }
 }
