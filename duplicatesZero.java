b1e65397871af04554790ac006fb47ebdf2d8022
iamLiwansh <saini.liwansh@gmail.com>
2024-09-23 21:04:52 +0530

    using itreative approch with n^2 TC

---
+++ duplicatesZero.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int len = arr.length;
+
+        for( int i = 0  ; i<len ; i++){
+            int temp = 0 ;
+            if(arr[i] == 0){
+                for(int j = len-1 ; j > i ; j--){
+                arr[j] = arr[j-1]; 
+                }
+                               
+            i++;
+            }
+        }
+
+    }
+}
